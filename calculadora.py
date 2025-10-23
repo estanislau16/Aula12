b3bf2870dec9f8a9f85d9e42882c53b987d535f5
Andre Estanislau <andredecesari@gmail.com>
2025-10-23 20:28:58 -0300

    Subir primeiro código

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+def funcaoSoma (num1, num2):
+    soma = num1+num2
+    return soma
+
+numA = input("Digite o primeiro numero")
+numB = input("Digite o segundo numero")
+
+print(funcaoSoma(numA,numB))
+
+def funcaoSubtrair(num1,num2):
+    subtrai = num1-num2
+    return subtrai
+
+numC = input("Digite o primeiro numero")
+numD = input("Digite o segundo numero")
+
+print(funcaoSubtrair(numC,numD))
+
+#subir para repositorio
